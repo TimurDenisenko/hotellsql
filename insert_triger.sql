ef376f11696ea4896fc867e12ab5b26991a1a114
TimurDenisenko <120181244+TimurDenisenko@users.noreply.github.com>
2023-09-04 10:56:14 +0300

    Update insert_triger.sql

--- insert_triger.sql
+++ insert_triger.sql
@@ -39,3 +39,46 @@ select * from guest;
 update guest set first_name='Timur' where guest_id=2;
 select * from guest;
 select * from logi;
+
+CREATE TRIGGER roomLisamine
+ON room 
+FOR INSERT
+AS
+INSERT INTO logi(kuupaev,kasutaja,andmed,tegevus)
+SELECT GETDATE(), USER, 
+CONCAT(inserted.number,', ',inserted.name,', ',' ,',inserted.status,', ',inserted.smoke),'room on lisatud'
+FROM inserted
+--kontroll
+INSERT INTO room(number,name,status,smoke) VALUES('505','akula','vaba',1);
+select * from room;
+select * from logi;
+
+CREATE TRIGGER roomKustuta
+ON room 
+FOR DELETE
+AS
+INSERT INTO logi(kuupaev,kasutaja,andmed,tegevus)
+SELECT GETDATE(), USER, 
+CONCAT(deleted.number,', ',deleted.name,', ',' ,',deleted.status,', ',deleted.smoke),'room on kustutatud'
+FROM deleted
+--kontroll
+select * from room;
+DELETE from room where room_id=3;
+select * from room;
+select * from logi;
+
+CREATE TRIGGER roomUuendamine
+ON room 
+FOR UPDATE
+AS
+INSERT INTO logi(kuupaev,kasutaja,andmed,tegevus)
+SELECT GETDATE(), USER, 
+CONCAT('vanad andmed: ',deleted.number,', ',deleted.name,', ',' ,',deleted.status,', ',deleted.smoke,
+'\n uued andmed: ',inserted.number,', ',inserted.name,', ',' ,',inserted.status,', ',inserted.smoke),'room on uuendatud'
+FROM deleted INNER JOIN inserted
+ON deleted.room_id=inserted.room_id
+--kontroll
+select * from room;
+update room set number='999' where room_id=4;
+select * from room;
+select * from logi;
